c27830a4983965c5ad3163ac76d8da8108e3af7a
Shadi Bitaraf <bitarafshh@gmail.com>
2023-06-04 19:58:18 -0700

    Using RandomizedSearchCV for values of k in kNN

--- fashion-mnist/classifiers.py
+++ fashion-mnist/classifiers.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-import torch # pytorch
 import tensorflow as tf
 
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import RandomizedSearchCV
 
 
 
@@ -18,21 +18,35 @@ from sklearn.metrics import accuracy_score
 # Flatten the image data
 train_images = train_images.reshape(-1, 28 * 28)
 test_images = test_images.reshape(-1, 28 * 28)
+# Create a kNN classifier
+#Using RandomizedSearchCV to perform random search with cross-validation, trying different k values and selecting the best one based on the accuracy score. 
+
+# Define the parameter grid for random search
+param_grid = {'n_neighbors': np.arange(1, 21)}
 
 # Create a kNN classifier
-k_values = [3, 5, 7, 9, 11]
-for k in k_values:
-    knn = KNeighborsClassifier(n_neighbors=k, n_jobs=-1) 
+knn = KNeighborsClassifier()
 
-    # Fit the classifier to the training data
-    knn.fit(train_images, train_labels)
+# Perform random search for best k value
+random_search = RandomizedSearchCV(knn, param_grid, cv=5, n_iter=10, scoring='accuracy')
+random_search.fit(train_images, train_labels)
 
-    # Make predictions on the test data
-    test_predictions = knn.predict(test_images)
+# Print the best k value and its corresponding test accuracy
+best_k = random_search.best_params_['n_neighbors']
+best_accuracy = random_search.best_score_
+print(f"Best k: {best_k}")
+print(f"Best Accuracy: {best_accuracy}")
 
-    # Calculate accuracy on the test data
-    test_accuracy = accuracy_score(test_labels, test_predictions)
-    print(f"kNN Classifier test Accuracy for k={k}: {test_accuracy}")
+# Create a kNN classifier with the best k value
+best_knn = KNeighborsClassifier(n_neighbors=best_k)
+best_knn.fit(train_images, train_labels)
+
+# Make predictions on the test data
+test_predictions = best_knn.predict(test_images)
+
+# Calculate accuracy on the test data
+test_accuracy = accuracy_score(test_labels, test_predictions)
+print(f"kNN Classifier test Accuracy for k={best_k}: {test_accuracy}")
 
 
 #Random Forest Classifier:
